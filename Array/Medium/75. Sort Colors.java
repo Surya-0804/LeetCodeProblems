325ef65d35fed1048a92ad355d7f602c44aa5290
Surya-0804 <159000250+Surya-0804@users.noreply.github.com>
2024-03-19 20:56:39 +0530

    Create 75. Sort Colors.java

---
+++ Array/Medium/75. Sort Colors.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int[] count=new int[3];
+        Arrays.fill(count,0);
+        for(int num : nums){
+            count[num]++;
+        }
+        int index=0;
+        for(int i=0;i<3;i++){
+            for(int j=0;j<count[i];j++){
+                nums[index++]=i;
+            }
+        }
+    }
+}
