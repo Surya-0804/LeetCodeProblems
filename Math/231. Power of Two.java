7e445865e39f6d4cdcad61605d56ed0dff003930
Surya-0804 <159000250+Surya-0804@users.noreply.github.com>
2024-03-21 21:17:04 +0530

    Create 231. Power of Two.java

---
+++ Math/231. Power of Two.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n<=0){
+            return false;
+        }
+        if(n==1){
+            return true;
+        }
+        if(n%2==1){
+            return false;
+        }
+        return isPowerOfTwo(n/2);
+    }
+}
