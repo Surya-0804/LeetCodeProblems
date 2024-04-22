68a9bb0b9ed0477051eea34ec4a40ae3d4f8f4b7
Surya-0804 <159000250+Surya-0804@users.noreply.github.com>
2024-04-22 20:27:09 +0530

    Create 169. Majority Element.java

---
+++ Array/Easy/169. Majority Element.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int num : nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+            if(map.get(num)>nums.length/2)
+                return num;
+        }
+        return -1;
+    }
+}
