93eaa79f288d238dccd9d1af94e10db75211ecd3
Surya-0804 <159000250+Surya-0804@users.noreply.github.com>
2024-04-23 09:51:00 +0530

    Create 258. Add Digits.java

---
+++ Math/Easy/258. Add Digits.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int addDigits(int num) {
+        int sum=0;
+        while(num>0){
+            sum +=num%10;
+            num/=10;
+        }
+        return (sum<10)?sum:addDigits(sum);
+    }
+}
