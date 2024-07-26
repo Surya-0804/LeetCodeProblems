6adec6e831b171a9d861cf1eececcc42274a3144
Ram Sai Sri Surya Abothula <159000250+Surya-0804@users.noreply.github.com>
2024-07-26 22:25:10 +0530

    Create 190. Reverse Bits.java

---
+++ Math/Easy/190. Reverse Bits.java
@@ -0,0 +1,16 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        if(n == 0)
+            return 0;
+        
+        int result=0;
+        for(int i=0; i<32; i++){
+            result<<=1;
+            if((n&1)==1)
+                result++;
+            n>>=1;
+        }
+        return result;
+    }
+}
