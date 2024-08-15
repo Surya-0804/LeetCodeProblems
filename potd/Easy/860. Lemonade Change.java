ac676a715e3e697e88f157f7548de7c5e1901b1d
Ram Sai Sri Surya Abothula <159000250+Surya-0804@users.noreply.github.com>
2024-08-15 20:00:11 +0530

    Create 860. Lemonade Change.java

---
+++ potd/Easy/860. Lemonade Change.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int count5=0;
+        int count10=0;
+        for(int bill : bills){
+            if(bill==5)
+                count5+=1;
+            else if(bill==10){
+                if(count5>0){
+                    count5--;
+                    count10++;
+                }
+                else
+                    return false;
+            }
+            else {
+                if(count5>0 && count10 >0){
+                    count10--;
+                    count5--;
+                }else if(count5>2){
+                    count5-=3;
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
